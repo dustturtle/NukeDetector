19ec8d75f3a89f741eb55a583fceda8b9caa9ce1
Zhenwei Guan <openglnewbee@163.com>
2019-01-05 15:18:43 +0800

    使用了5个样本，对比测试了不同的变换操作下模型的结果。测试结果表明，我们的代码（不做任何变换）产生的结果优于原git库里的实现；因此我们可以认为对于框架的设计使用来说，使用不做变换的输入是合适的。

--- NukeDetector/ViewController.m
+++ NukeDetector/ViewController.m
@@ -118,7 +118,40 @@
                                                  rgbColorSpace,
                                                  (CGBitmapInfo)kCGImageAlphaNoneSkipFirst);
     NSParameterAssert(context);
-    CGContextConcatCTM(context, CGAffineTransformIdentity);
+    
+    // gzw todo: 记录数据！ 1/2/3。
+    //1.直接使用标准变换 CGContextConcatCTM(context, CGAffineTransformIdentity) = 不加任何变换
+    //2.加上下面的三种变换： Rotation + flipVertical + flipHorizontal
+    //3.注释掉flipHorizontal
+    // 测试用pic A B C D..（默认取左上） 注：为避免不必要的问题，这里不把样本贴出。
+    // pic A:纯黄图，上半身，无衣物
+    // pic B:上半身，有胸罩，不能算严格的黄图
+    // pic C:正常人物图，非黄
+    // pic D:擦边球类型的性感图，取右上。
+    // pic E:大概率认定为黄图，左侧有物体遮挡。
+    // results:(A/B/C/D/E)
+    // 1. 97.76黄/56.44黄/99.57正常/93.10黄/90.39黄
+    // 2. 89.97黄/71.70黄/97.48正常/71.09黄/54.96正常
+    // 3. 92.48黄/64.39黄/93.62正常/81.03黄/55.44正常
+    //
+    //结论：对于无疑的判例A和C，1的置信度更高，结果更可信;
+    //对于判例B, 2和3更高概率认定为黄图，不理想;
+    //对于判例D, 1的置信度更高（三种都认定其为黄图，取0.7的时候），我们认为也没有问题。
+    //对于判例E, 输入的画面基本全裸体，1认定为黄图，另外两种认定为正常；1是最理想的。
+    
+    // 实验表明转换会降低精度。说明这里的输入应该不转换为宜。？？！！！
+    // 不同的输入，不同的结果！
+    
+    CGContextConcatCTM(context, CGAffineTransformMakeRotation(0)); //0
+    CGAffineTransform flipVertical = CGAffineTransformMake( 1, 0, 0, -1, 0, CGImageGetHeight(cgRef));
+    CGContextConcatCTM(context, flipVertical);
+
+//    CGAffineTransform flipHorizontal = CGAffineTransformMake( -1.0, 0.0, 0.0, 1.0, CGImageGetWidth(cgRef), 0.0);
+//    CGContextConcatCTM(context, flipHorizontal);
+    
+    //gzw
+    //CGContextConcatCTM(context, CGAffineTransformIdentity);
+    
     CGContextDrawImage(context, CGRectMake(0,
                                            0,
                                            frameWidth,
